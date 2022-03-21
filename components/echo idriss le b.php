503a5b4d584947fe94f1b7f9113d3adf25952d40
RobinSartini <robinsartini5@gmail.com>
2022-03-21 21:48:34 +0100

    test

---
+++ components/echo idriss le b.php
@@ -0,0 +1,1 @@
+echo idriss le b
---
+++ constants/data.js
@@ -0,0 +1,93 @@
+const userData = {
+  githubUsername: "RobinSartini",
+  name: "Robin Sartini",
+  designation: "étudiant en BTS-SIO",
+  avatarUrl: "/avatar.png",
+  email: "robinsartini@gmail.com",
+  phone: "06 50 30 99 69",
+  address: "3 rue de Turin, Villeurbanne,France",
+  projects: [
+    {
+      title: "Mon Youtube",
+      link: "https://www.youtube.com/channel/UCHZ9yYKJdNR4vd4jcxWp86w",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "Mon Instagram",
+      link: "https://www.instagram.com/robin_sartini/?hl=fr",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Mon tweeter",
+      link: "https://twitter.com/Shiroamaa",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "Mon discord",
+      link: "https://discord.com",
+      imgUrl: "/covidrescue.png",
+    },
+
+  ],
+  about: {
+    title:
+      "Je suis un simple étudiant qui aime le code ! retrouver mon travail sur se site...",
+    description: [""
+      
+    ],
+    currentProject: " my portfolio !",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Mon Premier stage",
+      company: "Eventio",
+      year: "2021",
+      desc: "Premier cours avec Tobias !",
+    },
+    {
+      title: "BTS-SIO",
+      company: "la martinière duchère",
+      year: "2021",
+      companyLink: "https://www.martiniere-duchere.fr",
+      desc: "Actuellement en première année de me BTS-SIO à la martinière duchère",
+    },
+    {
+      title: "Mon premier job",
+      company: "Mc Donalds",
+      year: "2020",
+      companyLink: "https://www.mcdonalds.fr",
+      desc: "Je me devait de trouver un job étudiant alors j'ai pris l'entreprise qui recrutait le plus à se moment la !"
+    },
+    {
+      title: "Mes Années de Fac",
+      company: "Université Lyon 1",
+      year: "2019",
+      companyLink: "https://www.univ-lyon1.fr",
+      desc: "J'ai étudié en pcsi pendant 2ans avant de me réorienté ",
+    },
+    {
+      title: "Lycée",
+      company: "Lycée Frédéric Fays à Villeurbanne",
+      year: "2019",
+      companyLink: "https://lyc-frederic-fays.ent.auvergnerhonealpes.fr",
+      desc: "J'ai éffectuer toute mes années dans se lycée avec un bac STI2D que j'ai eu a la fin des mes études .",
+    },
+    {
+      title: "Collège",
+      company: "Collège des Iris à Villeurbanne",
+      year: "2015",
+      companyLink: "https://reseaulesiris.blogs.laclasse.com",
+      desc: "C'est la que j'ai fais toute ma scolarité",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/robin_sartini/?hl=fr",
+    twitter: "https://twitter.com/Shiroamaa",
+    github: "Il arrive bientot !",
+  },
+};
+
+export default userData;
